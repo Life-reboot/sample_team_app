7c8408a97440527e589d143ef729dc226e58f424
“dtugui” <mirojoan89@yahoo.com>
2020-05-03 12:00:14 -0500

    Fixed merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/test" => "test#index"
-    get "/pages" => "pages#index"
-<<<<<<< HEAD
     get "/tests" => "tests#index"
-=======
+    get "/pages" => "pages#index"
+
+    get "/tests" => "tests#index"
+
     get "/something" => "something#index"
     get "/fish" => "fish#index"
     get "/jangs" => "jangs#index"
     get "/derulo" => "derulo#index"
     get "/bierdeman" => "bierdeman#index"
->>>>>>> 35ac2e099dc453eb5ad150461d7ed830901d2dc4
+
   end
 end
